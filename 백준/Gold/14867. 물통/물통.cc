7a678f41edb15caad0df06a8375184cee7b4ec4f
canyos <31244128+canyos@users.noreply.github.com>
2024-08-29 22:11:25 +0900

    Update 물통.cc

--- 백준/Gold/14867. 물통/물통.cc
+++ 백준/Gold/14867. 물통/물통.cc
@@ -27,30 +27,30 @@ int main()
 		long long temp = ll(v.first, v.second);
 		dq.pop();
 		//cout << v.first << " " << v.second << " " << mp[temp] << " " << temp << endl;
-		if (v.first == c && v.second == d)
+		if (v.first == c && v.second == d)//같으면 출력
 		{
 			cout << mp[temp]-1;
 			return 0;
 		}
 		
-		if (v.first < a && !mp[ll(a,v.second)])
+		if (v.first < a && !mp[ll(a,v.second)]) //a를 채울 수 있으면 채움
 		{
 			dq.push({ a,v.second });
 			mp[ll(a, v.second)] = mp[temp] + 1;
 		}
-		if (v.second < b && !mp[ll(v.first, b)])
+		if (v.second < b && !mp[ll(v.first, b)]) //b를 채울 수 있으면 채움
 		{
 			dq.push({ v.first,b });
 			mp[ll(v.first, b)] = mp[temp] + 1;
 		}
-		if (v.first > 0 && v.second != b)
+		if (v.first > 0 && v.second != b)//(a->b로 이동할 거임)
 		{
-			if (v.first - min(v.first, b - v.second) == 0 && !mp[ll(v.first - min(v.first, b - v.second), min(v.first + v.second, b))])
+			if (v.first - min(v.first, b - v.second) == 0 && !mp[ll(v.first - min(v.first, b - v.second), min(v.first + v.second, b))]) //a에 b의 빈 공간보다 물이 더 많음
 			{
-				dq.push({ v.first - min(v.first,b - v.second),min(v.first + v.second,b) });
+				dq.push({ v.first - min(v.first,b - v.second),min(v.first + v.second,b) }); 
 				mp[(ll(v.first - min(v.first, b - v.second), min(v.first + v.second, b)))] = mp[temp] + 1;
 			}
-			else
+			else //b에 공간이 더 적음
 				if (!mp[ll(v.first - min(v.first, b - v.second), min(v.first + v.second, b))])
 				{
 					dq.push({ v.first - min(v.first,b - v.second),min(v.first + v.second,b) });
@@ -58,9 +58,10 @@ int main()
 				}
 					
 		}
-		if (v.second > 0 && v.first != a)
+		if (v.second > 0 && v.first != a) b->a로 이동할 거임
+			
 		{
-			if (v.second - min(v.second, a - v.first) == 0 && !mp[ll(min(v.first + v.second, a), v.second - min(v.second, a - v.first))])
+			if (v.second - min(v.second, a - v.first) == 0 && !mp[ll(min(v.first + v.second, a), v.second - min(v.second, a - v.first))]) //b에 a의 빈 공간보다 양이 많음
 			{
 				dq.push({ min(v.first + v.second,a),v.second - min(v.second,a - v.first) });
 				mp[ll(min(v.first + v.second, a), v.second - min(v.second, a - v.first))] = mp[temp] + 1;
@@ -74,12 +75,12 @@ int main()
 				}
 					
 		}
-		if (v.first > 0 && !mp[ll(0, v.second)])
+		if (v.first > 0 && !mp[ll(0, v.second)]) //a를 버림
 		{
 			dq.push({ 0,v.second });
 			mp[ll(0, v.second)] = mp[temp] + 1;
 		}
-		if (v.second > 0 && !mp[ll(v.first,0)])
+		if (v.second > 0 && !mp[ll(v.first,0)]) //b를 버림
 		{
 			dq.push({  v.first,0 });
 			mp[ll(v.first, 0)] = mp[temp] + 1;
