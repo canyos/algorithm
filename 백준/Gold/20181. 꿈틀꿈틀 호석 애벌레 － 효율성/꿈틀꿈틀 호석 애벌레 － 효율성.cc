062ce04d3cd722937c451aafb501e037d526681e
canyos <31244128+canyos@users.noreply.github.com>
2024-08-29 18:06:11 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 8 ms, Memory: 8312 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.cc
@@ -0,0 +1,73 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <stack>
+#include <queue>
+
+using namespace std;
+int N, M, K;
+int arr[100001];
+class stick {
+public:
+	int start;
+	int end;
+	int value;
+
+	stick(int start, int end, int value) {
+		this->start = start;
+		this->end = end;
+		this->value = value;
+	}
+};
+
+vector<stick> sticks[100001];
+int dp[100001];
+
+void input() {
+	cin >> N  >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+	}
+}
+
+void tp() {
+	int left = 1, right = 1;
+	int sum = arr[1];
+	while (right <= N) {
+		if (sum >= K) {
+			sticks[right].push_back(stick(left, right, sum - K));
+			sum -= arr[left];
+			left++;
+		}
+		else {
+			right++;
+			sum += arr[right];
+		}
+	}
+}
+
+void DP() {
+	for (int i = 1; i <= N; i++) {
+		int temp = 0;
+		for (int j = 0; j < (int)sticks[i].size(); j++) {
+			temp = max(temp, dp[sticks[i][j].start - 1] + sticks[i][j].value);
+		}
+		dp[i] = max(dp[i - 1], temp);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	input();
+	tp();
+	DP();
+
+	cout << dp[N];
+	return 0;
+}
