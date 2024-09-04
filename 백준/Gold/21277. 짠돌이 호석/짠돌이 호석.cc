3e4e4bba762e8148d032f4588f0a461d89fb25e3
canyos <31244128+canyos@users.noreply.github.com>
2024-09-04 17:50:35 +0900

    [Gold III] Title: 짠돌이 호석, Time: 76 ms, Memory: 2052 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.cc
+++ 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.cc
@@ -1,111 +1,66 @@
-#include <string>
-#include <vector>
-#include <cmath>
 #include <iostream>
 #include <algorithm>
-#include <deque>
-#include <stack>
-#include <queue>
-#include <map>
-
+#include <string>
+#define NM 105
+#define INF 0x7fffffff
+#define vi vector<int>
+FILE* in = stdin, * out = stdout;
 using namespace std;
-typedef long long ll;
 
-int N, M, N2, M2;
-string temp;
-bool arr[51][51];
-bool arr2[4][51][51];
-int result;
+int n1, m1, n2, m2;
+char a[NM][NM], b[NM][NM], temp[NM][NM];
+int ans = INF;
 
 void input() {
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		cin >> temp;
-		for (int j = 0; j < M; j++) {
-			arr[i][j] = temp[j] - '0';
-		}
-	}
-	cin >> N2 >> M2;
-	for (int i = 0; i < N2; i++) {
-		cin >> temp;
-		for (int j = 0; j < M2; j++) {
-			arr2[0][i][j] = temp[j] - '0';
-			arr2[1][j][N2 - 1 - i] = temp[j] - '0';
-			arr2[2][N2 - 1 - i][M2 - 1 - j] = temp[j] - '0';
-			arr2[3][M2 - 1 - j][i] = temp[j] - '0';
-		}
-	}
+    cin >> n1 >> m1;
+    for (int i = 1; i <= n1; i++) for (int j = 1; j <= m1; j++) cin >> a[i][j];
+    cin >> n2 >> m2;
+    for (int i = 1; i <= n2; i++) for (int j = 1; j <= m2; j++) cin >> b[i][j];
 }
-
-
-bool combine(int i, int j, int k) {
-	for (int a = 0; a + i < N; a++) {
-		for (int b = 0; b + j < M; b++) {
-			if (arr[a + i][b + j] & arr2[k][a][b])
-				return false;
-		}
-	}
-	return true;
+void rotate(char a[][NM], int& n, int& m) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            temp[j][n - i + 1] = a[i][j];
+        }
+    }
+    swap(n, m);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            a[i][j] = temp[i][j];
+        }
+    }
 }
-
-bool combine2(int i, int j, int k) {
-	for (int a = 0; a + i < (k % 2 == 0 ? N2 : M2); a++) {
-		for (int b = 0; b + j < (k % 2 == 0 ? M2 : N2); b++) {
-			if (arr[a][b] & arr2[k][a + i][b + j])
-				return false;
-		}
-	}
-	return true;
+bool possible(int shift_row, int shift_col) {
+    for (int ai = 1; ai <= n1; ai++) {
+        for (int aj = 1; aj <= m1; aj++) {
+            if (a[ai][aj] == '0')
+                continue;
+            int bi = ai + shift_row, bj = aj + shift_col;
+            if (1 <= bi && bi <= n2 && 1 <= bj && bj <= m2 && b[bi][bj] == '1')
+                return false;
+        }
+    }
+    return true;
+}
+void pro() {
+    for (int rot = 1; rot <= 4; rot++) {
+        rotate(a, n1, m1);
+        for (int shift_row = -51; shift_row <= 51; shift_row++) {
+            for (int shift_col = -51; shift_col <= 51; shift_col++) {
+                if (possible(shift_row, shift_col)) {
+                    int row = max(n2 - 1, shift_row + n1 - 1) - min(0, shift_row) + 1;
+                    int col = max(m2 - 1, shift_col + m1 - 1) - min(0, shift_col) + 1;
+                    ans = min(ans, row * col);
+                }
+            }
+        }
+    }
+    cout << ans;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	input();
-
-
-	result = min((N + N2)*max(M, M2), (M + M2)*max(N, N2));
-	result = min(result, (N + M2)*max(M, N2));
-	result = min(result, (M + N2)*max(N, M2));//하나도 안겹쳤을때
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			for (int k = 0; k < 4; k++) {
-				if (combine(i, j, k)) {
-					if (k % 2) {//n2 <->m2 교체
-						result = min(result, max(N, i + M2)* max(M, j + N2));
-						//cout << i << " " << j << " " << k << " " << max(N, i + M2) << " " << max(M, j + N2) << endl;
-					}
-					else { //n2, m2그대로
-						result = min(result, max(N, i + N2)*max(M, j + M2));
-						//cout << i << " " << j << " " << k << " " << max(N, i + N2) << " " << max(M, j + M2) << endl;
-					}
-
-				}
-			}
-		}
-	}
-	
-	for (int k = 0; k < 4; k++) {//2번 그림 고정
-		for (int i = 0; i < (k % 2 == 0 ? N2 : M2); i++) {//회전 체크
-			for (int j = 0; j < (k % 2 == 0 ? M2 : N2); j++) {//회전 체크
-				if (combine2(i, j, k)) {
-					if (k % 2) {//n2 <->m2 교체
-						result = min(result, max(M + j, N2)* max(N + i, M2));
-						//cout << i << " " << j << " " << k << " " << max(N, i + M2) << " " << max(M, j + N2) << endl;
-					}
-					else { //n2, m2그대로
-						result = min(result, max(N + i, N2)*max(M + j, M2));
-						//cout << i << " " << j << " " << k << " " << max(N, i + N2) << " " << max(M, j + M2) << endl;
-					}
-
-				}
-			}
-		}
-		
-	}
-	cout << result;
-	return 0;
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    input();
+    pro();
+    return 0;
 }
