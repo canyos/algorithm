d17ee1892a13ba853c03f966cbf3a492de8ae481
canyos <31244128+canyos@users.noreply.github.com>
2025-08-22 14:27:36 +0900

    [Gold II] Title: 나누기, Time: 32 ms, Memory: 2800 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.cc
+++ 백준/Gold/21757. 나누기/나누기.cc
@@ -17,46 +17,34 @@ const ll INF = 1e9;
 
 int n, m;
 int arr[100001];
-ll dp[5][100001];
 int sum[100001];
-int quarter;
-vector<int> v[5];
+ll dp[5];
+int cnt0, base;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        sum[i] = sum[i - 1] + arr[i];
+        if (sum[i] == 0) cnt0++;
+    }
+    if (sum[n] == 0) {
+        if (cnt0 < 4) cout << "0";
+        else cout<<  1ll*(cnt0 - 1) * (cnt0 - 2) * (cnt0 - 3) / 6;
+        return 0;
+    }
+    if (sum[n] % 4) {
+        cout << "0";
+        return 0;
+    }
+    base = sum[n] / 4;
 
-	cin >> n; 
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	sum[0] = arr[0];
-	for (int i = 1; i < n; i++) 
-		sum[i] = sum[i - 1] + arr[i];
-	
-	quarter = sum[n - 1]/4;
-
-	for (int i = 0; i < n; i++) {
-		for (int k = 1; k <= 4; k++) {
-			if (quarter * k == sum[i])
-				v[k].push_back(i);
-		}
-	}
-
-	for (auto idx : v[1]) {
-		dp[1][idx] = 1;
-	}
-
-	for (int k = 2; k <= 4; k++) {
-		for (int idx : v[k]) {//합이 k인거
-			for (int prev : v[k - 1]) {
-				if (prev >= idx)break;
-				dp[k][idx] += dp[k - 1][prev];
-			}
-		}
-	}
-		
-	cout << dp[4][n - 1];
-
-
-	return 0;
+    dp[0] = 1;
+    for (int i = 1; i < n; i++) {
+        if (sum[i] % base) continue;
+        int j = sum[i] / base;
+        if (j == 0 || j > 3) continue;
+        dp[j] += dp[j - 1];
+    }
+    cout << dp[3];
 }
